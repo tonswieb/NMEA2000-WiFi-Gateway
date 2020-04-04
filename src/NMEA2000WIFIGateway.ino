afdf17a86b2fe7587c67cb891b7f48021a002723
Ton Swieb <ton.swieb@finalist.nl>
2020-04-04 19:57:28 +0200

    Move ResetWIFI to own file. Add TX port for NMEA38400.

--- src/NMEA2000WIFIGateway.ino
+++ src/NMEA2000WIFIGateway.ino
@@ -3,15 +3,15 @@
 // The code has been tested with ESP32.
 
 #define ESP32_NMEA38400_RX 5
+#define ESP32_NMEA38400_TX 13 //Free pin. We don't need to use, but need to map something.
 #define ESP32_CAN_TX_PIN GPIO_NUM_12
 #define ESP32_CAN_RX_PIN GPIO_NUM_13
 #include <NMEA2000_CAN.h>
 #include <WiFi.h>
-#include <nvs.h>
-#include <nvs_flash.h>
 #include "N2kDataToNMEA0183.h"
 #include <WiFiUdp.h>
 #include "SerialToNMEA0183.h"
+#include "ResetWifi.cpp"
 
 // Define your default settings here
 const char *ssid = "ARISTO"; //Replace with WIFI name.
@@ -19,41 +19,21 @@ const char *udpAddress = "192.168.4.255";
 const int port = 9876;
 WiFiUDP udp;
 SerialToNMEA0183* aisReceiver;
-
-bool ResetWiFiSettings=true; // If you have tested other code in your module, it may have saved settings and have difficulties to make connection.
-
 tN2kDataToNMEA0183 tN2kDataToNMEA0183(&NMEA2000, 0);
 
 // Forward declarations
 void SendNMEA0183Message(const tNMEA0183Msg &NMEA0183Msg);
 void SendNMEA0183Message(SerialToNMEA0183* serialToNMEA0183);
+void InitWIFI();
 void InitNMEA2000();
-
-#include <nvs.h>
-#include <nvs_flash.h>
-
-void ResetWiFiSettingsOnNvs() {
-  int err;
-  err=nvs_flash_init();
-  Serial.println("nvs_flash_init: " + err);
-  err=nvs_flash_erase();
-  Serial.println("nvs_flash_erase: " + err); 
-}
 
 //*****************************************************************************
 void setup() {
-  if ( ResetWiFiSettings ) ResetWiFiSettingsOnNvs();
-
   Serial.begin(115200);
-  Serial1.begin(38400, SERIAL_8N1, ESP32_NMEA38400_RX);
+  Serial1.begin(38400, SERIAL_8N1, ESP32_NMEA38400_RX, ESP32_NMEA38400_TX);
 
-  // Set WiFi to station mode and disconnect from an AP if it was previously connected
-  delay(1000);
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(ssid);
-  
+  InitWIFI();
   InitNMEA2000();
-  Serial.println("NMEA200 initialized.");
 
   aisReceiver = new SerialToNMEA0183(&Serial1);
 }
@@ -65,8 +45,18 @@ void loop() {
   SendNMEA0183Message(aisReceiver);
 }
 
+void InitWIFI() {
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  ResetWiFiSettingsOnNvs();
+  delay(1000);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid);
+}
+
 //*****************************************************************************
 void InitNMEA2000() {
+
   NMEA2000.SetN2kCANMsgBufSize(8);
   NMEA2000.SetN2kCANReceiveFrameBufSize(100);
   NMEA2000.SetForwardStream(&Serial);  // PC output on due native port
@@ -77,6 +67,7 @@ void InitNMEA2000() {
   tN2kDataToNMEA0183.SetSendNMEA0183MessageCallback(SendNMEA0183Message);
 
   NMEA2000.Open();
+  Serial.println("NMEA200 initialized.");
 }
 
 #define MAX_NMEA0183_MESSAGE_SIZE 100
