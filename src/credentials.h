00966c0ff226c62e357d0b4a5724e6f26d7c3207
Ton Swieb <ton.swieb@finalist.nl>
2020-04-11 15:07:00 +0200

    Remove credentials.h

--- src/credentials.h
+++
@@ -1,5 +0,0 @@
-/*
-Put you personal credentials here and add this file to you .gitignore file
-*/
-const char *ssid = "NicE_Engineering_UPC"; //Replace with WIFI name.
-const char *password = "1001100110";
