29b86aa029e698f789b27cc8124d3ad77b429a0c
Ton Swieb <ton.swieb@finalist.nl>
2020-08-17 23:03:38 +0200

    Variation must be initialized, because used in toMagnetic calculation

--- src/nmea/N183ToN2k.h
+++ src/nmea/N183ToN2k.h
@@ -32,7 +32,8 @@ class N183ToN2k {
 
   private:  
     unsigned long DaysSince1970;   // Days since 1970-01-01
-    double Variation, Latitude, Longitude;
+    double Latitude, Longitude;
+    double Variation = 0;
     //NMEA0183-BOD (Bearing Origin to Destination). Does not conatin enough information to send a NMEA200 message, but contains some elements required for a NMEA2000 message.
     tBOD bod;
     Route* route;
